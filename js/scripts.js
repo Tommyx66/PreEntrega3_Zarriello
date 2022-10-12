825c14212a2e4a6166101bfc422c6b592442c491
Tommyx66 <tomz.ry64@gmail.com>
2022-10-12 08:08:40 -0300

    Resubida del Proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,3 @@ let menu_bar =  document.querySelector ('#menu-bar');
 
 	})
 
-/*--- Boton Leer mas ---*/
-
-let readMore_btn = document.getElementById('readMore_btn');
-let hideText = document.getElementById('hideText');
-
-readMore_btn.addEventListener('click', toggleText);
-
-function toggleText() {
-  hideText.classList.toggle('showText');
-
-  if(hideText.classList.contains('showText')) {
-    readMore_btn.innerHTML = 'Leer Menos'
-  }
-  else {
-    readMore_btn.innerHTML = 'Leer Más'
-  }
-}
