a06ff37b0919cd3bded68a0b41cd5b948dd3b5a7
Tommyx66 <tomz.ry64@gmail.com>
2022-10-18 20:08:36 -0300

    3erPreEntrega

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ let menu =  document.querySelector ('#menu');
 
 let menu_bar =  document.querySelector ('#menu-bar');
 
-   menu_bar.addEventListener ('click', function() {
+menu_bar.addEventListener ('click', function() {
 
-		menu.classList.toggle('menu-toggle');
+	menu.classList.toggle('menu-toggle');
 
-	})
+})
 
